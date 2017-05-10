448fe7e9eb8296fae1ebe00216a7666453b61c84
fnixTiago <israel.only23@gmail.com>
2017-05-09 21:10:03 -0500

    version 2

--- lab_9.cpp
+++ lab_9.cpp
@@ -6,27 +6,7 @@
 using namespace std;
 typedef char T;
 
-void generara_tabla(map<T,map<T, int> > &Tabla){
-	Tabla['A']['A']=10;		Tabla['A']['G']=-1;		Tabla['A']['C']=-3;		Tabla['A']['T']=-4;	
-	Tabla['G']['A']=-1;		Tabla['G']['G']=7;		Tabla['G']['C']=-5;		Tabla['G']['T']=-3;	
-	Tabla['C']['A']=-3;		Tabla['C']['G']=-5;		Tabla['C']['C']=9;		Tabla['C']['T']=0;	
-	Tabla['T']['A']=-4;		Tabla['T']['G']=-3;		Tabla['T']['C']=0;		Tabla['T']['T']=8;	
-}
-
-void mostrar_matriz(map<T, map<T,int> >&m){
-    for (auto it : m) {
-        cout << it.first << " : ";
-        map<T, int> internal_map = it.second;
-        for (auto it2: internal_map) {
-            cout <<"["<< it.first<<"]["<<it2.first << "]:" << it2.second<<"\t\t";
-        }
-        cout << endl;
-    }  
-}
-
 void generar_matriz(vector<vector<int> > &M, string cad1, string cad2,int match, int mismatch, int gap_penalty){
-	
-
 	M.resize(cad1.size()+1);
 	for (int i = 0; i < M.size(); ++i){
 		M[i].resize(cad2.size()+1);
@@ -53,9 +33,7 @@ void generar_matriz(vector<vector<int> > &M, string cad1, string cad2,int match,
 			v.clear();
 			score =0;
 		}
-
 	}
-
 	cout<<"\nMOSTRAR\n";
 	cout<<"\t";
 	for (int i = 0; i < cad2.size(); ++i){
@@ -71,7 +49,6 @@ void generar_matriz(vector<vector<int> > &M, string cad1, string cad2,int match,
 		cout<<cad1[i]<<"\t";
 	}
 	cout<<endl;
-
 }
 
 int position_max(vector<int>&v){
@@ -89,21 +66,19 @@ int position_max(vector<int>&v){
 void alineamiento(vector<vector<int> > &M, string cad1, string cad2, int match, int mismatch, int gap_penalty){
 	//encontrar el maximo
 	int position_mayor =position_max(M[M.size()-1]);
-	vector<pair<char,char>> alineado;
-	int i =M.size()-1;
+	
+	vector<pair<char,char>> alineado;//va contener las alineaciones de las 2 cadenas
+	int i =M.size()-1;//recuenda que se empieza desde 0
 	int j =position_mayor;
 	pair<char,char> pareja;
-	//cout<<"\nHOLA\n";
 	while(i>0 && j>0){
 		int score =(cad1[i-1]==cad2[j-1])?match:mismatch;
-		//cout<<"\nSCORE: "<<score<<endl;
 		if((M[i-1][j-1] + score)== M[i][j]){//diagonal
 			pareja =make_pair(cad1[i-1],cad2[j-1]);
 			alineado.push_back(pareja);
 			//cout<<"\nDIAGONAL\n";
 			i--;
 			j--;
-		//	printf("(i,j): (%d,%d)\n",i,j);
 		}
 		else if((M[i-1][j] + gap_penalty)== M[i][j]){//arriba
 			pareja =make_pair(cad1[i-1],'_');
@@ -123,50 +98,18 @@ void alineamiento(vector<vector<int> > &M, string cad1, string cad2, int match, 
 	for (int i = 0; i < alineado.size(); ++i){
 		cout<<alineado[i].first<<" - "<<alineado[i].second<<endl;	
 	}
-	if(i>0)
-		for (int k = i; k < 0; --k)
-		{
-			cout<<cad1[k-1]<<" - "<<"_"<<endl;
-		}
-	if(j>0)
-		for (int k = i; k < 0; --k)
-		{
-			cout<<"_"<<" - "<<cad1[k-1]<<endl;
-		}
-
-
-
 	//limpieza de la matriz
-
 	for (int i = 0; i < M.size(); ++i)
 	{
 		M[i].clear();
 	}
-
 }
 
 int main(){
-	map<T,map<T, int> > tabla;
-	vector<vector<int> > M;
+	vector<vector<int> > M;//matriz
 	int match,mismatch,gap_penalty;
-//	match =5;//match si los caracteres son iguales
-//	mismatch =-3;// si no son iguales
-//	gap_penalty =-4;//se suma a la diagonal
-
-
-
-	generara_tabla(tabla);
-	//mostrar_matriz(tabla);
-	
 	string cad1,cad2;
-/*	cad1= "GGATCGA";
-	cad2= "GAATTCAGTTA";
-	cout<<"\nFilas: "<<cad1.size()+1<<endl;
-	cout<<"\nColumnas: "<<cad2.size()+1<<endl;
-	generar_matriz(M,cad1,cad2,match,mismatch,gap_penalty);
-	alineamiento(M,cad1,cad2,match,mismatch,gap_penalty);
-	cout<<"\nEXITO\n";
-*/
+
 	cout<<"\nEjemplo 1\n";
 	cad1= "GAT";
 	cad2= "GGAT";
@@ -193,7 +136,6 @@ int main(){
 	gap_penalty = -2;
 	generar_matriz(M,cad1,cad2,match,mismatch,gap_penalty);
 	alineamiento(M,cad1,cad2,match,mismatch,gap_penalty);
-
 	return 0;
 }
 
